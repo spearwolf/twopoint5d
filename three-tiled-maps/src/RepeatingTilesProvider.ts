27385c7d5a09f8b58bdf976e720201f5b5a66fcc
Wolfger Schramm <wolfger@spearwolf.de>
2021-06-09 10:13:35 +0200

    create RepeatingTilesProvider constructor

--- three-tiled-maps/src/RepeatingTilesProvider.ts
+++ three-tiled-maps/src/RepeatingTilesProvider.ts
@@ -8,10 +8,30 @@ export type LimitToAxisType = "horizontal" | "vertical" | "none";
  * If you want you can limit the repeat to only horizontal or only vertical.
  */
 export class RepeatingTilesProvider implements IMap2dTileDataProvider {
+  tileIds: number[][];
+  limitToAxis: LimitToAxisType;
+
   constructor(
-    tileIds: RepeatingTilesPatternType,
+    tileIds?: RepeatingTilesPatternType,
     limitToAxis: LimitToAxisType = "none"
-  ) {}
+  ) {
+    if (typeof tileIds === "number") {
+      this.tileIds = [[tileIds]];
+    } else if (Array.isArray(tileIds)) {
+      if (typeof tileIds[0] === "number") {
+        this.tileIds = [tileIds as number[]];
+      } else if (
+        Array.isArray(tileIds[0]) &&
+        typeof tileIds[0][0] === "number"
+      ) {
+        this.tileIds = tileIds as number[][];
+      }
+    }
+    if (!this.tileIds) {
+      this.tileIds = [[]];
+    }
+    this.limitToAxis = limitToAxis;
+  }
 
   getTileIdsWithin(
     left: number,
