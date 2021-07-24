88b2b0541ecffcf7ee9b403ac3b7d301ea01f561
Wolfger Schramm <wolfger@spearwolf.de>
2021-07-24 10:11:56 +0200

    doc(three-tiled-maps): add doc to Map2dAreaTile

--- three-tiled-maps/src/Map2dAreaTile.ts
+++ three-tiled-maps/src/Map2dAreaTile.ts
@@ -1,5 +1,11 @@
 import {AABB2} from './AABB2';
 
+/**
+ * The `Map2dAreaTile` addresses a single tile in a tile grid.
+ * Has `x` and `y` coordinates (as tile coordinates) and an `id`,
+ * as well as an {@link AABB2} (as real world coordinates).
+ * Used in {@link Map2dLayer} for identifying tiles.
+ */
 export class Map2dAreaTile {
   static createID(x: number, y: number) {
     return `y${y.toString(16)}${x < 0 ? '' : 'x'}${x.toString(16)}`;
