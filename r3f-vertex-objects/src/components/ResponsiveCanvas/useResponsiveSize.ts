c7dc613ba4d1d12d4d960c191802fb10aa78e50f
Wolfger Schramm <wolfger@spearwolf.de>
2021-08-09 13:04:30 +0200

    r3f-vertex-objects: fix null ref issue

--- r3f-vertex-objects/src/components/ResponsiveCanvas/useResponsiveSize.ts
+++ r3f-vertex-objects/src/components/ResponsiveCanvas/useResponsiveSize.ts
@@ -10,35 +10,37 @@ export function useResponsiveSize(containerRef: RefObject<HTMLElement>) {
     const onFrame = () => {
       rafId = requestAnimationFrame(onFrame);
 
-      const domElement = containerRef.current;
-      const {width, height} = domElement.getBoundingClientRect();
+      const domElement = containerRef?.current;
+      if (domElement) {
+        const {width, height} = domElement.getBoundingClientRect();
+        const styles = getComputedStyle(domElement, null);
 
-      const styles = getComputedStyle(domElement, null);
-      const verticalMargin =
-        parseInt(styles.getPropertyValue('border-top-width') || '0', 10) +
-        parseInt(styles.getPropertyValue('border-bottom-width') || '0', 10) +
-        parseInt(styles.getPropertyValue('padding-top') || '0', 10) +
-        parseInt(styles.getPropertyValue('padding-bottom') || '0', 10);
-      const horizontalMargin =
-        parseInt(styles.getPropertyValue('border-right-width') || '0', 10) +
-        parseInt(styles.getPropertyValue('border-left-width') || '0', 10) +
-        parseInt(styles.getPropertyValue('padding-left') || '0', 10) +
-        parseInt(styles.getPropertyValue('padding-right') || '0', 10);
+        const verticalMargin =
+          parseInt(styles.getPropertyValue('border-top-width') || '0', 10) +
+          parseInt(styles.getPropertyValue('border-bottom-width') || '0', 10) +
+          parseInt(styles.getPropertyValue('padding-top') || '0', 10) +
+          parseInt(styles.getPropertyValue('padding-bottom') || '0', 10);
+        const horizontalMargin =
+          parseInt(styles.getPropertyValue('border-right-width') || '0', 10) +
+          parseInt(styles.getPropertyValue('border-left-width') || '0', 10) +
+          parseInt(styles.getPropertyValue('padding-left') || '0', 10) +
+          parseInt(styles.getPropertyValue('padding-right') || '0', 10);
 
-      const wPx = Math.floor(width - horizontalMargin);
-      const hPx = Math.floor(height - verticalMargin);
+        const wPx = Math.floor(width - horizontalMargin);
+        const hPx = Math.floor(height - verticalMargin);
 
-      const dpr = window.devicePixelRatio || 1;
+        const dpr = window.devicePixelRatio || 1;
 
-      if (
-        wPx !== sizeRef.current.wPx ||
-        hPx !== sizeRef.current.hPx ||
-        dpr !== sizeRef.current.dpr
-      ) {
-        sizeRef.current.wPx = wPx;
-        sizeRef.current.hPx = hPx;
-        sizeRef.current.dpr = dpr;
-        setSize({width: wPx, height: hPx, dpr});
+        if (
+          wPx !== sizeRef.current.wPx ||
+          hPx !== sizeRef.current.hPx ||
+          dpr !== sizeRef.current.dpr
+        ) {
+          sizeRef.current.wPx = wPx;
+          sizeRef.current.hPx = hPx;
+          sizeRef.current.dpr = dpr;
+          setSize({width: wPx, height: hPx, dpr});
+        }
       }
     };
 
