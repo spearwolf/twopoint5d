1b9468b6f5dedfd0d5e163a7555848ed9c3109f9
Wolfger Schramm <wolfger@spearwolf.de>
2022-10-12 09:59:45 +0200

    fix strange tsconfig warning for setup-tests.js

--- packages/picimo/jest.config.cjs
+++ packages/picimo/jest.config.cjs
@@ -123,7 +123,7 @@ module.exports = {
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
   // setupFiles: [],
-  setupFiles: ['./src/setup-tests.js'],
+  setupFiles: ['./setup-tests.js'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
