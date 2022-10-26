9cfd391d73484b728e0b2722a17f22753fb4ad1a
Wolfger Schramm <wolfger@spearwolf.de>
2022-10-26 12:08:21 +0200

    remove legacy stage25 pkg dir and add Stage2D docs

--- packages/stage25/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './fitIntoRectangle';
-export * from './IProjection';
-export * from './OrthographicProjection';
-export * from './ParallaxProjection';
-export * from './ProjectionPlane';
-export * from './Stage2D';
