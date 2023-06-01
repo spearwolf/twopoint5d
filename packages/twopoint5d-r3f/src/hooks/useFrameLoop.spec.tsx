5069791782b16b0cd36c2573b7bb455ca7215f33
Wolfger Schramm <wolfger@spearwolf.de>
2023-06-01 10:57:50 +0200

    improve useFrameLoop tests

--- packages/twopoint5d-r3f/src/hooks/useFrameLoop.spec.tsx
+++ packages/twopoint5d-r3f/src/hooks/useFrameLoop.spec.tsx
@@ -6,8 +6,6 @@ import {useRef} from 'react';
 import {Mesh} from 'three';
 import {forwardRefValue, useFrameLoop} from './useFrameLoop';
 
-// TODO test useFrameStateMachine(callbacks, /* without-extra-params */)
-
 const TestMesh = ({extraValue, callbacks}: any) => {
   const meshRef = useRef<Mesh>();
 
@@ -23,6 +21,23 @@ const TestMesh = ({extraValue, callbacks}: any) => {
 
 const TestScene = ({showMesh, extraValue, callbacks}: any) => (
   <>{showMesh && <TestMesh callbacks={callbacks} extraValue={extraValue} />}</>
+);
+
+const TestMeshWithoutDependencies = ({callbacks}: any) => {
+  const meshRef = useRef<Mesh>();
+
+  useFrameLoop({...callbacks});
+
+  return (
+    <mesh ref={meshRef as any}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshBasicMaterial color={0xffff00} />
+    </mesh>
+  );
+};
+
+const TestSceneWithoutDependencies = ({showMesh, callbacks}: any) => (
+  <>{showMesh && <TestMeshWithoutDependencies callbacks={callbacks} />}</>
 );
 
 describe('useFrameLoop hook', () => {
@@ -62,6 +77,108 @@ describe('useFrameLoop hook', () => {
     expect(args.mesh).toBe(mesh);
     expect(args.state).toBeDefined();
     expect(args.delta).toBe(1);
+    expect(args.foo).toBe('plah!');
+    expect(args.extraValue).toBe('abc');
+
+    callbacks.init.mockClear();
+
+    await ReactThreeTestRenderer.act(async () => {
+      renderer.advanceFrames(5, 2);
+    });
+
+    expect(callbacks.init).not.toBeCalled();
+    expect(callbacks.update).not.toBeCalled();
+    expect(callbacks.frame).toBeCalledTimes(5);
+    expect(callbacks.dispose).not.toBeCalled();
+
+    args = callbacks.frame.mock.calls.at(-1)[0];
+    expect(args.mesh).toBe(mesh);
+    expect(args.state).toBeDefined();
+    expect(args.delta).toBe(2);
+    expect(args.foo).toBe('plah!');
+    expect(args.extraValue).toBe('abc');
+
+    callbacks.frame.mockClear();
+
+    await renderer.update(<TestScene showMesh callbacks={{...callbacks}} extraValue="xyz" />);
+
+    expect(callbacks.init).not.toBeCalled();
+    expect(callbacks.update).not.toBeCalled();
+    expect(callbacks.frame).not.toBeCalled();
+    expect(callbacks.dispose).not.toBeCalled();
+
+    await ReactThreeTestRenderer.act(async () => {
+      renderer.advanceFrames(1, 1);
+    });
+
+    expect(callbacks.init).not.toBeCalled();
+    expect(callbacks.update).toBeCalledTimes(1);
+    expect(callbacks.frame).toBeCalledTimes(1);
+    expect(callbacks.dispose).not.toBeCalled();
+
+    args = callbacks.update.mock.calls[0][0];
+    expect(args).toStrictEqual({extraValue: {currentValue: 'xyz', previousValue: 'abc'}});
+
+    args = callbacks.frame.mock.calls.at(-1)[0];
+    expect(args.mesh).toBe(mesh);
+    expect(args.state).toBeDefined();
+    expect(args.delta).toBe(1);
+    expect(args.foo).toBe('plah!');
+    expect(args.extraValue).toBe('xyz');
+
+    callbacks.frame.mockClear();
+    callbacks.update.mockClear();
+
+    await renderer.update(<TestScene callbacks={callbacks} />);
+
+    await ReactThreeTestRenderer.act(async () => {
+      renderer.advanceFrames(1, 1);
+    });
+
+    expect(callbacks.init).not.toBeCalled();
+    expect(callbacks.update).not.toBeCalled();
+    expect(callbacks.frame).not.toBeCalled();
+    expect(callbacks.dispose).toBeCalledTimes(1);
+
+    args = callbacks.dispose.mock.calls[0][0];
+    expect(args.mesh).toBe(mesh);
+    expect(args.state).toBeUndefined();
+    expect(args.delta).toBeUndefined();
+    expect(args.foo).toBe('plah!');
+    expect(args.extraValue).toBe('xyz');
+  });
+
+  test('callbacks (without dependencies) are called in the correct sequence', async () => {
+    const callbacks = {
+      init: jest.fn(),
+      update: jest.fn(),
+      frame: jest.fn(),
+      dispose: jest.fn(),
+    };
+
+    const renderer = await ReactThreeTestRenderer.create(<TestSceneWithoutDependencies showMesh callbacks={callbacks} />);
+    const mesh = renderer.scene.findByType('Mesh').instance;
+
+    expect(mesh).toBeDefined();
+    expect(mesh.type).toBe('Mesh');
+
+    expect(callbacks.init).not.toBeCalled();
+    expect(callbacks.update).not.toBeCalled();
+    expect(callbacks.frame).not.toBeCalled();
+    expect(callbacks.dispose).not.toBeCalled();
+
+    await ReactThreeTestRenderer.act(async () => {
+      renderer.advanceFrames(1, 1);
+    });
+
+    expect(callbacks.init).toBeCalledTimes(1);
+    expect(callbacks.update).not.toBeCalled();
+    expect(callbacks.frame).not.toBeCalled();
+    expect(callbacks.dispose).not.toBeCalled();
+
+    let args = callbacks.init.mock.calls[0][0];
+    expect(args.state).toBeDefined();
+    expect(args.delta).toBe(1);
 
     await ReactThreeTestRenderer.act(async () => {
       renderer.advanceFrames(5, 2);
@@ -73,30 +190,32 @@ describe('useFrameLoop hook', () => {
     expect(callbacks.dispose).not.toBeCalled();
 
     args = callbacks.frame.mock.calls.at(-1)[0];
-    expect(args.mesh).toBe(mesh);
     expect(args.state).toBeDefined();
     expect(args.delta).toBe(2);
 
-    // await renderer.update(<TestScene showMesh callbacks={callbacks} extraValue="xyz" />);
+    await renderer.update(<TestSceneWithoutDependencies showMesh callbacks={callbacks} />);
 
-    // await ReactThreeTestRenderer.act(async () => {
-    //   renderer.advanceFrames(1, 1);
-    // });
-
-    // expect(callbacks.init).toBeCalledTimes(1);
-    // expect(callbacks.update).toBeCalled();
-    // expect(callbacks.frame).toBeCalledTimes(6);
-    // expect(callbacks.dispose).not.toBeCalled();
-
-    await renderer.update(<TestScene callbacks={callbacks} />);
+    await ReactThreeTestRenderer.act(async () => {
+      renderer.advanceFrames(1, 1);
+    });
 
     expect(callbacks.init).toBeCalledTimes(1);
-    // expect(callbacks.update).toBeCalledTimes(1);
-    expect(callbacks.frame).toBeCalledTimes(5);
+    expect(callbacks.update).not.toBeCalled();
+    expect(callbacks.frame).toBeCalledTimes(6);
+    expect(callbacks.dispose).not.toBeCalled();
+
+    await renderer.update(<TestSceneWithoutDependencies callbacks={callbacks} />);
+
+    await ReactThreeTestRenderer.act(async () => {
+      renderer.advanceFrames(1, 1);
+    });
+
+    expect(callbacks.init).toBeCalledTimes(1);
+    expect(callbacks.update).not.toBeCalled();
+    expect(callbacks.frame).toBeCalledTimes(6);
     expect(callbacks.dispose).toBeCalledTimes(1);
 
     args = callbacks.dispose.mock.calls.at(-1)[0];
-    expect(args.mesh).toBe(mesh);
     expect(args.state).toBeUndefined();
     expect(args.delta).toBeUndefined();
   });
