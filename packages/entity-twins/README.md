105fa70ccf3666c1052650d6ecc05d410fe3d343
Wolfger Schramm <wolfger@spearwolf.de>
2023-06-15 11:49:35 +0200

    create entity-twins library skeleton

---
+++ packages/entity-twins/README.md
@@ -0,0 +1,11 @@
+# twopoint5d
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build twopoint5d` to build the library.
+
+## Running unit tests
+
+Run `nx test twopoint5d` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/entity-twins/rollup.config.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+
+import {createBanner} from '../../rollup/createBanner.mjs';
+import {makeVersionWithBuild} from '../../rollup/makeVersionWithBuild.mjs';
+
+const projectDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)));
+const packageJson = JSON.parse(fs.readFileSync(path.join(projectDir, 'package.json')));
+
+const buildDir = path.join(projectDir, 'build');
+const distDir = path.join(projectDir, 'dist');
+
+const makeBanner = (build) => {
+  const version = makeVersionWithBuild(build)(packageJson.version);
+  return createBanner({...packageJson, version});
+};
+
+export default {
+  plugins: [nodeResolve()],
+  input: {
+    'entity-twins': path.join(buildDir, 'src/index.js'),
+  },
+  output: [
+    {
+      banner: makeBanner('esm'),
+      dir: distDir,
+      entryFileNames: '[name].js',
+      format: 'es',
+    },
+    {
+      banner: makeBanner('cjs'),
+      dir: distDir,
+      entryFileNames: '[name].cjs.js',
+      format: 'commonjs',
+      exports: 'named',
+    },
+  ],
+  external: packageJson.rollup?.external,
+};
