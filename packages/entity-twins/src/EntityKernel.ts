86a2404d1860fd5bccdeddb8ea46dd22537fd556
Wolfger Schramm <wolfger@spearwolf.de>
2023-06-19 12:30:39 +0200

    entity-twins: add property signals

--- packages/entity-twins/src/EntityKernel.ts
+++ packages/entity-twins/src/EntityKernel.ts
@@ -1,3 +1,4 @@
+import {batch} from '@spearwolf/signalize';
 import {EntityUplink} from './EntityUplink';
 import {EntitiesSyncEvent, EntityChangeType, IEntityChangeCreateEntity, IEntityChangeEntry} from './types';
 
@@ -5,29 +6,34 @@ export class EntityKernel {
   #entities: Map<string, EntityUplink> = new Map();
 
   run(event: EntitiesSyncEvent) {
-    // console.log('run', event);
-    for (const entry of event.changeTrail) {
-      this.parse(entry);
-    }
+    batch(() => {
+      for (const entry of event.changeTrail) {
+        this.parse(entry);
+      }
+    });
   }
 
   parse(entry: IEntityChangeEntry) {
     switch (entry.type) {
       case EntityChangeType.CreateEntity:
         this.createEntity(entry as IEntityChangeCreateEntity);
+        break;
+      // TODO implement other change-entry types
     }
   }
 
   createEntity(entry: IEntityChangeCreateEntity) {
     const entity = new EntityUplink(this, entry.uuid);
+
     // TODO use token for entity creation
-    // TODO respect entity parent
-    entry.properties?.forEach(([key, value]) => {
-      (entity as any)[key] = value;
-    });
+    // TODO respect entity parent -> children
+
+    if (entry.properties) {
+      entity.setProperties(entry.properties);
+    }
 
     this.#entities.set(entry.uuid, entity);
 
-    console.log('create', entity);
+    console.log('create', entity.uuid, entity.propertyEntries());
   }
 }
--- packages/entity-twins/src/EntityUplink.ts
+++ packages/entity-twins/src/EntityUplink.ts
@@ -1,9 +1,12 @@
 import {Eventize} from '@spearwolf/eventize';
+import {SignalReader, SignalWriter, batch, createSignal, value} from '@spearwolf/signalize';
 import {EntityKernel} from './EntityKernel';
 
 export class EntityUplink extends Eventize {
   #kernel: EntityKernel;
   #uuid: string;
+
+  #signals = new Map<string, [get: SignalReader<any>, set: SignalWriter<any>]>();
 
   get kernel(): EntityKernel {
     return this.#kernel;
@@ -17,5 +20,46 @@ export class EntityUplink extends Eventize {
     super();
     this.#kernel = kernel;
     this.#uuid = uuid;
+  }
+
+  getSignal<T = unknown>(key: string): [SignalReader<T>, SignalWriter<T>] {
+    if (!this.#signals.has(key)) {
+      const signal = createSignal<T>();
+      this.#signals.set(key, signal);
+      return signal;
+    }
+    return this.#signals.get(key)!;
+  }
+
+  getSignalReader<T = unknown>(key: string): SignalReader<T> {
+    return this.getSignal<T>(key)[0];
+  }
+
+  getSignalWriter<T = unknown>(key: string): SignalWriter<T> {
+    return this.getSignal<T>(key)[1];
+  }
+
+  setProperties(properties: [string, unknown][]) {
+    batch(() => {
+      for (const [key, val] of properties) {
+        this.setProperty(key, val);
+      }
+    });
+  }
+
+  setProperty<T = unknown>(key: string, value: T) {
+    this.getSignalWriter<T>(key)(value);
+  }
+
+  getProperty<T = unknown>(key: string): T {
+    return value(this.getSignalReader<T>(key));
+  }
+
+  propertyKeys(): string[] {
+    return Array.from(this.#signals.keys());
+  }
+
+  propertyEntries(): [string, unknown][] {
+    return Array.from(this.#signals.entries()).map(([key, [get]]) => [key, value(get)]);
   }
 }
