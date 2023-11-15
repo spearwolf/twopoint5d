f1a26c03d149b49c7b1d587e4b9fdf6da697b707
Wolfger Schramm <wolfger@spearwolf.de>
2023-11-15 13:37:41 +0100

    use headless mode in playwright

--- packages/twopoint5d-elements-e2e/playwright.config.ts
+++ packages/twopoint5d-elements-e2e/playwright.config.ts
@@ -36,17 +36,17 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: {...devices['Desktop Chrome']},
+      use: {...devices['Desktop Chrome'], headless: true},
     },
 
     {
       name: 'firefox',
-      use: {...devices['Desktop Firefox']},
+      use: {...devices['Desktop Firefox'], headless: true},
     },
 
     {
       name: 'webkit',
-      use: {...devices['Desktop Safari']},
+      use: {...devices['Desktop Safari'], headless: true},
     },
 
     /* Test against mobile viewports. */
