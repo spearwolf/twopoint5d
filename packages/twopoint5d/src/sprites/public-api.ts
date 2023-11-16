784cb73b298e9324ccba662f11c7e9760f138e85
Wolfger Schramm <wolfger@spearwolf.de>
2023-11-16 13:15:42 +0100

    move ShaderTool to sprites/

--- packages/twopoint5d/src/sprites/public-api.ts
+++ packages/twopoint5d/src/sprites/public-api.ts
@@ -3,6 +3,7 @@ export * from './AnimatedSprites.js';
 export * from './AnimatedSpritesGeometry.js';
 export * from './AnimatedSpritesMaterial.js';
 export * from './BaseSprite.js';
+export * from './ShaderTool.js';
 export * from './TexturedSprite.js';
 export * from './TexturedSprites.js';
 export * from './TexturedSpritesGeometry.js';
