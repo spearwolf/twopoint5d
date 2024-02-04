2b37e47f0a4e9e4e4ecf71981bc61f0285b76825
Wolfger Schramm <wolfger@spearwolf.de>
2024-02-04 16:22:25 +0100

    refactor lookbook theme

--- apps/lookbook/tailwind.config.js
+++ apps/lookbook/tailwind.config.js
@@ -1,4 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  plugins: [require('@tailwindcss/aspect-ratio')],
   content: ['./src/**/*.{astro,html,js,ts}'],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        ['sidebar-content']: '25rem auto',
+        ['only-content']: 'auto',
+        ['demo-cards']: 'repeat(auto-fill, minmax(30ch, 1fr))',
+      },
+    },
+  },
 };
