646a96387a85994ff6689afad86a1a9a9200a47c
Wolfger Schramm <wolfger@spearwolf.de>
2025-01-03 11:00:23 +0100

    remove landing-page skeleton

--- apps/landing-page/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  plugins: [require('@tailwindcss/aspect-ratio')],
-  content: ['./src/**/*.{astro,html,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-};
