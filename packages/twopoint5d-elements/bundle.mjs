46f73f799e693dc7acbc18183c9d2b9aff3d67f1
Wolfger Schramm <wolfger@spearwolf.de>
2025-03-14 11:55:35 +0100

    twopoint5d-elements: export elements.js

--- packages/twopoint5d-elements/bundle.mjs
+++ packages/twopoint5d-elements/bundle.mjs
@@ -10,12 +10,24 @@ const projectDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)));
 const buildDir = path.join(projectDir, 'build');
 const distDir = path.join(projectDir, 'dist');
 
-await esbuild.build({
-  entryPoints: [path.join(buildDir, 'src/bundle.js')],
-  bundle: true,
-  minify: true,
-  format: 'esm',
-  target: ['es2017'],
-  banner: {js: makeBanner(projectDir, 'bundle')},
-  outfile: `${distDir}/bundle.js`,
-});
+await Promise.all([
+  esbuild.build({
+    entryPoints: [path.join(buildDir, 'src/bundle.js')],
+    bundle: true,
+    minify: true,
+    format: 'esm',
+    target: ['es2017'],
+    banner: {js: makeBanner(projectDir, 'bundle')},
+    outfile: `${distDir}/bundle.js`,
+  }),
+  esbuild.build({
+    entryPoints: [path.join(buildDir, 'src/bundle.js')],
+    bundle: true,
+    packages: 'external',
+    minify: true,
+    format: 'esm',
+    target: ['esnext'],
+    banner: {js: makeBanner(projectDir, 'elements')},
+    outfile: `${distDir}/elements.js`,
+  }),
+]);
