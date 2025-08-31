475ce5143883ddfd94f5519a6b74077f13965eb3
Wolfger Schramm <wolfger@spearwolf.de>
2025-08-31 13:01:35 +0200

    lookbook: persist active tags locally

---
+++ apps/lookbook/src/components/constants.ts
@@ -0,0 +1,7 @@
+export const EVENT_SHOW_DEMOS = 'lookbook.showDemos';
+
+export const STORAGE_KEY_ACTIVE_TAGS = 'lookbook.activeTags';
+export const STORAGE_KEY_RELATED_TAGS = 'lookbook.relatedTags';
+
+export const STORAGE_KEY_SHOW_ALL_DEMOS = 'lookbook.demos.showAll';
+export const STORAGE_KEY_FILTER_DEMOS_BY_ID = 'lookbook.demos.filterById';
